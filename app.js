fe564f9bfca927027be4c0d23a51ab40c2671b43
Ranjeet Singh <44586579+jeetsdev@users.noreply.github.com>
2022-01-14 12:21:10 +0530

    Refactor code

--- app.js
+++ app.js
@@ -5,42 +5,47 @@ const btnCross = document.querySelector(".cross-btn");
 const outputPara = document.querySelector(".output-para");
 const privacyPara = document.querySelector(".privacy");
 const imgElement = document.querySelector(".img-output");
-btnFind.addEventListener('click', () => {
-    let sumOfBirthday = 0;
-    let birthInput = userBirthday.value;
-    let numInput = userNumber.value;
-    console.log(birthInput);
-    console.log(numInput);
-    if (birthInput === '' || numInput === "") {
-        outputPara.innerHTML = "Enter some value first.";
-        outputPara.style.color = 'red';
-        imgElement.src = './images/think.png';
-        return;
-    }
-    birthInput = birthInput.replaceAll("-", '');
-    birthInput = parseInt(birthInput);
-    numInput = parseInt(numInput);
-    if (numInput<1) {
-        outputPara.innerHTML = "Lucky number must be grater than 0";
-        outputPara.style.color = 'red';
-        imgElement.src = './images/think.png';
-        return;
-    }
-    else{
-        while (birthInput) {
-        sumOfBirthday += (birthInput % 10);
-        birthInput = Math.floor(birthInput / 10);
+btnFind.addEventListener("click", () => {
+  let sumOfBirthday = 0;
+  let birthInput = userBirthday.value;
+  let numInput = userNumber.value;
+
+  if (birthInput === "" || numInput === "") {
+    setStyle("Enter some value first.", "red", "./images/think.png");
+    return;
+  }
+  birthInput = birthInput.replaceAll("-", "");
+  birthInput = parseInt(birthInput);
+  numInput = parseInt(numInput);
+  if (numInput < 1) {
+    setStyle("Lucky number must be grater than 0", "red", "./images/think.png");
+    return;
+  } else {
+    while (birthInput) {
+      sumOfBirthday += birthInput % 10;
+      birthInput = Math.floor(birthInput / 10);
     }
     if (sumOfBirthday % numInput === 0) {
-        outputPara.innerHTML = "Yay 👏 ! Your birthday is lucky. 🎉🎉";
-        outputPara.style.color = 'rgb(1, 255, 77)';
-        imgElement.src = './images/smiley.png';
+      setStyle(
+        "Yay 👏 ! Your birthday is lucky. 🎉🎉",
+        "rgb(1, 255, 77)",
+        "./images/smiley.png"
+      );
     } else {
-        outputPara.innerHTML = "Opps! Your birthday is not lucky. 🙁🙁"
-        outputPara.style.color = 'black';
-        imgElement.src = '/images/sad.png';
-    }}
-})
-btnCross.addEventListener('click', () => {
-    privacyPara.style.visibility = "hidden";
-})
+      setStyle(
+        "Opps! Your birthday is not lucky. 🙁🙁",
+        "black",
+        "/images/sad.png"
+      );
+    }
+  }
+});
+btnCross.addEventListener("click", () => {
+  privacyPara.style.visibility = "hidden";
+});
+
+function setStyle(paraText, paraColor, imgSrc) {
+  outputPara.innerHTML = `${paraText}`;
+  outputPara.style.color = `${paraColor}`;
+  imgElement.src = `${imgSrc}`;
+}
